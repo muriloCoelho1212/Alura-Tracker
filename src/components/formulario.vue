5949c951a2b79a7ecb1a2576b52c29e5716308ad
Murilo <m.coelhos1212@gmail.com>
2023-09-13 12:48:44 -0300

    commit final aula 2

--- src/components/formulario.vue
+++ src/components/formulario.vue
@@ -2,59 +2,34 @@
     <div class="box">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao">
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>
-                            {{ tempoDecorrido }}
-                        </strong>
-                    </section>
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>Play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>Stop</span>
-                    </button>
-                </div>
+                <TemporizadorForm @ao-temporizador-finalizado="finalizarTarefa"/>
             </div>
         </div>
     </div>
 </template>
 <script lang="ts">
     import { defineComponent } from 'vue';
+    import TemporizadorForm from './temporizador.vue'
 
     export default defineComponent({
-        name: 'FormPrincipal',
-        data(){
-            return {
-                tempoEmSegundos: 0,
-                cronometro: 0
-            }
-        },
-        computed: {
-            tempoDecorrido (): string{
-                return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19)
-            }
-        },
-        methods: {
-            iniciar(){
-                this.cronometro = setInterval(() => {
-                    this.tempoEmSegundos += 1
-                }, 1000)
-            },
-            finalizar(){
-                clearInterval(this.cronometro)
-            }
+    name: 'FormPrincipal',
+    components: { TemporizadorForm},
+    data(){
+        return {
+            descricao: ''
         }
-    })
+    },
+    methods: {
+        finalizarTarefa(tempoDecorrido: number): void{
+            console.log(tempoDecorrido)
+            console.log(this.descricao)
+            this.descricao = ''
+        }
+    }
+})
 </script>
 <style>
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-import '@fortawesome/fontawesome-free'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 createApp(App).mount('#app')
